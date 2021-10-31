51a5ff3d3c381aa3e50e38b449487481e25101dc
kevin olson <acidjazz@gmail.com>
2021-10-30 21:23:02 -0500

    🚧 general progress

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,24 @@
+import 'unocss'
 import { defineNuxtConfig } from 'nuxt3'
+import UnoCSS from 'unocss/vite'
+import UnocssIcons from '@unocss/preset-icons'
 
 export default defineNuxtConfig({
     components: true,
     buildModules: [
         'nuxt-windicss',
     ],
+    css: [
+        '~/styles/main.css',
+    ],
+    vite: {
+        build: {},
+        plugins: [
+            UnoCSS({
+                presets: [
+                    UnocssIcons(),
+                ],
+            }),
+        ],
+    },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="flex items-center justify-center w-screen h-screen bg-gray-100">
+  <div class="flex flex-col items-center justify-center w-screen h-screen">
+    <icon-fume class="w-24 h-24" />
     <div class="p-8 bg-white rounded-lg shadow">
-      <icon-fume class="w-24 h-24" />
+      <div>
+        <div>
+          <div class="i-carbon-campsite" />
+        </div>
+        <div>
+          Server-Side Rendering
+        </div>
+      </div>
     </div>
   </div>
 </template>
