ab1ec970a812d29b4f9780d657a8538a96f4beaa
kevin olson <acidjazz@gmail.com>
2021-11-03 22:24:18 -0500

    🔧 add a sample environment variable

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,43 @@
   <div class="flex flex-col items-center justify-center w-screen h-screen">
     <icon-fume class="w-36 h-36" />
     <div class="bg-white rounded-lg shadow flex flex-col divide-y">
+
       <div class="flex items-center p-2 space-x-2">
         <div class="i-bi-check-lg w-6 h-6 text-green-500" />
         <div>Server-Side Rendering ({{ version }}) </div>
       </div>
+
       <div class="flex items-center p-2 space-x-2">
         <img src="/nuxt.svg" class="w-6 h-6" alt="Nuxt Image">
         <div>Statically cached image</div>
       </div>
+
+      <div class="flex items-center p-2 space-x-2">
+        <div class="i-bx-bxl-aws w-6 h-6 text-yellow-700" />
+        <div>Build test number ({{ number }}) </div>
+      </div>
+
+      <!--
+      <div class="flex items-center p-2 space-x-2">
+        <div class="i-fa-solid:cog w-6 h-6 text-gray-400" />
+        <div>process . env . API_URL: {{ API_URL }} </div>
+      </div>
+      -->
+
+      <pre>
+        {{ json }}
+      </pre>
+
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useAsyncData } from '#app'
-
 let version = (await useAsyncData('version', () => process.version)).data
-
+const number = ref(<number>16)
+const json = ref(<undefined|string>undefined)
+onMounted( () => {
+  json.value = JSON.stringify(process, undefined, '\t')
+})
 </script>
