e9c07e20cc017582cc15e15260119aac85201954
kevin olson <acidjazz@gmail.com>
2021-11-04 02:24:36 -0500

    ♻️ remove older code - swap icons

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
     <div class="bg-white rounded-lg shadow flex flex-col divide-y">
 
       <div class="flex items-center py-2 px-4 space-x-4">
-        <div class="i-bi-check-lg w-6 h-6 text-green-500" />
-        <div>Server-Side Rendering ({{ version }}) </div>
+        <div class="i-bi-server w-6 h-6 text-blue-500" />
+        <div>Server-Side Rendering  (node {{ version }}) </div>
       </div>
 
       <div class="flex items-center py-2 px-4 space-x-4">
@@ -31,8 +31,4 @@
 import { useAsyncData } from '#app'
 let version = (await useAsyncData('version', () => process.version)).data
 const number = ref(<number>16)
-const json = ref(<undefined|string>undefined)
-onMounted( () => {
-  json.value = JSON.stringify(process, undefined, '\t')
-})
 </script>
